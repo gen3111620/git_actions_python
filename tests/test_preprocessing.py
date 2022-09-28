1b3755d8e88ac2ffa5ac1f26030a3ca6483593a7
arvissu <gen3111620@gmail.com>
2022-09-28 15:42:43 +0800

    add test case

---
+++ tests/test_preprocessing.py
@@ -0,0 +1,14 @@
+import sys
+import numpy as np
+import pandas as pd
+import pytest
+
+sys.path.append("../")
+
+from git_actions_python.preprocessing import preprocessing
+
+
+def test_preprocessing():
+    df = pd.read_csv("train.csv")
+    df = preprocessing(df)
+    assert np.all(df.Sex.values[:5] == np.array([0, 1, 1, 1, 0], dtype=np.int64))
