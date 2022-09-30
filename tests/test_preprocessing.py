612342c7014622b70d4e09608506db7a51f4bdc7
arvissu <gen3111620@gmail.com>
2022-09-30 10:09:42 +0800

    test2

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -10,6 +10,6 @@ from preprocessing import preprocessing
 #unit test
 
 def test_preprocessing():
-    df = pd.read_csv("../train.csv")
+    df = pd.read_csv("train.csv")
     df = preprocessing(df)
     assert np.all(df.Sex.values[:5] == np.array([0, 1, 1, 1, 0], dtype=np.int64))
